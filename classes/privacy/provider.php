902c154e5194a2a93acdc6bfce253ab2f1cb2494
Tobias Garske <tobias.garske@isb.bayern.de>
2026-03-15 15:14:36 +0000

    Update privacy provider

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -24,21 +24,54 @@
 
 namespace format_mimo\privacy;
 
+use core_privacy\local\metadata\collection;
+use core_privacy\local\metadata\provider as metadata_provider;
+use core_privacy\local\request\user_preference_provider;
+
 /**
- * Privacy Subsystem for format_mimo implementing null_provider.
+ * Privacy Subsystem for format_mimo implementing user_preference_provider.
  *
  * @package    format_mimo
  * @copyright  2025 Tobias Garske
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class provider implements \core_privacy\local\metadata\null_provider {
+class provider implements metadata_provider, user_preference_provider {
     /**
-     * Get the language string identifier with the component's language
-     * file to explain why this plugin stores no data.
+     * Describe the type of personal data stored by this plugin.
      *
-     * @return string
+     * @param collection $collection The collection to add metadata to.
+     * @return collection The updated collection.
      */
-    public static function get_reason(): string {
-        return 'privacy:metadata';
+    public static function get_metadata(collection $collection): collection {
+        $collection->add_user_preference(
+            'format_mimo_lastsection',
+            'privacy:metadata:preference:lastsection'
+        );
+        return $collection;
+    }
+
+    /**
+     * Export all user preferences for the plugin.
+     *
+     * @param int $userid The userid of the user whose data is to be exported.
+     */
+    public static function export_user_preferences(int $userid): void {
+        global $DB;
+
+        $preferences = $DB->get_records_select(
+            'user_preferences',
+            $DB->sql_like('name', ':prefix') . ' AND userid = :userid',
+            ['prefix' => 'format\\_mimo\\_lastsection\\_%', 'userid' => $userid]
+        );
+
+        foreach ($preferences as $preference) {
+            $courseid = substr($preference->name, strlen('format_mimo_lastsection_'));
+            \core_privacy\local\request\writer::export_user_preference(
+                'format_mimo',
+                $preference->name,
+                $preference->value,
+                get_string('privacy:metadata:preference:lastsection', 'format_mimo', $courseid)
+            );
+        }
     }
 }
---
+++ tests/privacy_provider_test.php
@@ -0,0 +1,103 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace format_mimo;
+
+use core_privacy\local\metadata\collection;
+use core_privacy\local\request\writer;
+use core_privacy\tests\provider_testcase;
+use format_mimo\privacy\provider;
+
+/**
+ * Privacy provider tests for format_mimo.
+ *
+ * @package    format_mimo
+ * @copyright  2025 Tobias Garske
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @covers     \format_mimo\privacy\provider
+ */
+final class privacy_provider_test extends provider_testcase {
+
+    public function test_get_metadata(): void {
+        $collection = new collection('format_mimo');
+        $collection = provider::get_metadata($collection);
+
+        $items = $collection->get_collection();
+        $this->assertCount(1, $items);
+        $this->assertInstanceOf(\core_privacy\local\metadata\types\user_preference::class, $items[0]);
+    }
+
+    public function test_export_user_preferences_no_data(): void {
+        $this->resetAfterTest();
+        $user = $this->getDataGenerator()->create_user();
+        $this->setUser($user);
+
+        provider::export_user_preferences($user->id);
+
+        $writer = writer::with_context(\context_system::instance());
+        $prefs = $writer->get_user_preferences('format_mimo');
+
+        $this->assertEmpty((array) $prefs);
+    }
+
+    public function test_export_user_preferences_with_data(): void {
+        $this->resetAfterTest();
+        $user = $this->getDataGenerator()->create_user();
+        $this->setUser($user);
+
+        $course = $this->getDataGenerator()->create_course(['format' => 'mimo']);
+
+        set_user_preference('format_mimo_lastsection_' . $course->id, 3, $user);
+
+        provider::export_user_preferences($user->id);
+
+        $writer = writer::with_context(\context_system::instance());
+        $prefs = $writer->get_user_preferences('format_mimo');
+
+        $prefkey = 'format_mimo_lastsection_' . $course->id;
+        $this->assertObjectHasProperty($prefkey, $prefs);
+        $this->assertEquals(3, $prefs->$prefkey->value);
+        $this->assertEquals(
+            get_string('privacy:metadata:preference:lastsection', 'format_mimo', $course->id),
+            $prefs->$prefkey->description
+        );
+    }
+
+    public function test_export_user_preferences_multiple_courses(): void {
+        $this->resetAfterTest();
+        $user = $this->getDataGenerator()->create_user();
+        $this->setUser($user);
+
+        $course1 = $this->getDataGenerator()->create_course(['format' => 'mimo']);
+        $course2 = $this->getDataGenerator()->create_course(['format' => 'mimo']);
+
+        set_user_preference('format_mimo_lastsection_' . $course1->id, 2, $user);
+        set_user_preference('format_mimo_lastsection_' . $course2->id, 5, $user);
+
+        provider::export_user_preferences($user->id);
+
+        $writer = writer::with_context(\context_system::instance());
+        $prefs = $writer->get_user_preferences('format_mimo');
+
+        $key1 = 'format_mimo_lastsection_' . $course1->id;
+        $key2 = 'format_mimo_lastsection_' . $course2->id;
+
+        $this->assertObjectHasProperty($key1, $prefs);
+        $this->assertObjectHasProperty($key2, $prefs);
+        $this->assertEquals(2, $prefs->$key1->value);
+        $this->assertEquals(5, $prefs->$key2->value);
+    }
+}
